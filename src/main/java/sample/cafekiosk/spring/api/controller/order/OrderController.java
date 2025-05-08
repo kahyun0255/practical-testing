bc6ae2180cf68634f204c24aa4112f2c18466d28
kahyun0255 <kkh9946@naver.com>
2025-05-08 22:26:42 +0900

    test: 주문 생성 테스트 추가

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.api.controller.order;
 
+import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public ApiResponse<OrderResponse> createOrder(@RequestBody OrderCreateRequest request){
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request){
         LocalDateTime registeredDateTime = LocalDateTime.now();
         return ApiResponse.ok(orderService.createOrder(request, registeredDateTime));
     }
