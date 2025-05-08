5db8b540f8c8f736eeac76771ca4607d5960b72a
kahyun0255 <kkh9946@naver.com>
2025-05-08 22:21:58 +0900

    fix: 주문 생성 기능 ApiResponse로 주도록 변경

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.controller.order.response.OrderResponse;
 import sample.cafekiosk.spring.api.service.order.OrderService;
@@ -16,8 +17,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public OrderResponse createOrder(@RequestBody OrderCreateRequest request){
+    public ApiResponse<OrderResponse> createOrder(@RequestBody OrderCreateRequest request){
         LocalDateTime registeredDateTime = LocalDateTime.now();
-        return orderService.createOrder(request, registeredDateTime);
+        return ApiResponse.ok(orderService.createOrder(request, registeredDateTime));
     }
 }
